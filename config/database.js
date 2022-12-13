d4fd7855216530f9475e6d0569adf6c03b408cf3
Lil-Korzhik <mr.purple2007@gmail.com>
2022-12-14 01:18:41 +0200

    Change database on MySQL

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const path = require('path');
+/* const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
@@ -7,5 +7,19 @@ module.exports = ({ env }) => ({
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', 'data.db')),
     },
     useNullAsDefault: true,
+  },
+}); */
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('MYSQLHOST', '127.0.0.1'),
+      port: env.int('MYSQLPORT', 3306),
+      database: env('MYSQLDATABASE', 'mypartyzebra'),
+      user: env('MYSQLUSER', 'root'),
+      password: env('MYSQLPASSWORD', ''),
+      ssl: env.bool(true)
+    }
   },
 });
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -10,11 +10,21 @@
         password: env('PGPASSWORD', 'password'),
         ssl: env.bool(true)
       }
+    },
+    acquireConnectionTimeout: 5000,
+    pool: {
+      min: 0,
+      max: 10,
+      createTimeoutMillis: 8000,
+      acquireTimeoutMillis: 8000,
+      idleTimeoutMillis: 8000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100,
     }
 }); */
 
 // ---- MySQL ----
-/* module.exports = ({ env }) => ({
+module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
@@ -25,18 +35,5 @@
       password: env('MYSQLPASSWORD', 'password'),
       ssl: env.bool(true)
     }
-  },
-}); */
-
-// ---- SQLite ----
-const path = require('path');
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', 'data.db')),
-    },
-    useNullAsDefault: true,
   },
 });
