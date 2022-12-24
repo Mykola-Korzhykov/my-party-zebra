cffbcae946fc9cd3112cf18ee094674b4ab390d2
Lil-Korzhik <mr.purple2007@gmail.com>
2022-12-25 00:14:29 +0200

    Fix the bug with font

--- next.config.js
+++ next.config.js
@@ -48,37 +48,6 @@ const getConfig = async () => {
       }
 
       return config;
-    },
-    async headers() {
-      return [
-        {
-          source: "/fonts/Zebra-Regular.woff2",
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "public, max-age=31536000, immutable",
-            },
-          ],
-        },
-        {
-          source: "/fonts/Zebra-Medium.woff2",
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "public, max-age=31536000, immutable",
-            },
-          ],
-        },
-        {
-          source: "/fonts/Zebra-Bold.woff2",
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "public, max-age=31536000, immutable",
-            },
-          ],
-        }
-      ];
     }
   })
 }
