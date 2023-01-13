0e3728d9acfd8a38e7541bbabfb58de47f666e1a
Lil-Korzhik <mr.purple2007@gmail.com>
2023-01-13 01:36:21 +0100

    Create 'Organize'

---
+++ src/api/page-organize/controllers/page-organize.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-organize controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::page-organize.page-organize');
---
+++ src/api/page-organize/routes/page-organize.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-organize router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::page-organize.page-organize');
